d16a1cc9770df9c2e5e8789f809fb4072b12637e
Julio Tavares <juliotavares197@gmail.com>
2023-07-09 18:55:58 -0300

    feat: :zap: change to fastify and use swc compiler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '@/users/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [],
   providers: [],
 })
