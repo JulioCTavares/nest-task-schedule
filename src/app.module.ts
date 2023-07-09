d6fa09bc0ec280c13cca36b2e05199342d8bac5f
Julio Tavares <juliotavares197@gmail.com>
2023-07-09 19:12:51 -0300

    feat: :sparkles: Add docker-compose and prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '@/users/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
